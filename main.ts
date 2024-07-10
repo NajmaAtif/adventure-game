6a8876b46a2f5f748b6739fec9026a5e503bb9ee
NajmaAtif <164073896+NajmaAtif@users.noreply.github.com>
2024-07-10 13:41:10 +0500

    my first commit

---
+++ main.ts
@@ -0,0 +1,103 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+// Games Variable
+let enemies = ["Skeleton","Zombie","Warrior","Assassin"]
+let maxEnemyHealth = 75
+let enemyAttackDamageToHero = 25
+
+
+// Player Variable
+let heroHealth = 100
+let attackDamageToEnemy = 50
+let numHealthPotion = 3
+let healthPotionHealAmount = 30
+let healthPotionDropChance = 50
+
+
+// while loop condition
+
+let gameRunning = true
+
+console.log("Welcome to DeadZone!");
+
+Game:
+while (gameRunning){
+    let enemyHealth = Math.floor(Math.random()* maxEnemyHealth + 1)
+    let enemyIndex = Math.floor(Math.random() *enemies.length)
+    let enemy = enemies[enemyIndex]
+
+    console.log(`# ${enemy} has appeared #\n`);
+
+    while (enemyHealth>0){
+        console.log(`Your Health ${heroHealth}`);
+        console.log(`${enemy} Health: ${enemyHealth}`);
+
+        let options = await inquirer.prompt({
+         name: "ans",
+         type: "list",
+         message: "What would you like to do?",
+         choices: ["1. Attack", "2.Take Health Potion","3.Run"]
+        })
+        if()
+    }
+
+}
+
+
+
+
+
+
+
+
+// // classes player & opponent
+// class Player{
+//     name:string;
+//     fuel: number = 100;
+//     constructor(name:string){
+//         this.name=name;
+//     }
+// }
+
+// class Opponent {
+//     name:string;
+//     fuel: number = 100; 
+//     constructor(name:string){
+//         this.name=name
+//     }
+// }
+// // Player name & opponent select
+// let player = await inquirer.prompt({
+//     type:"input",
+//     name:"name",
+//     message: "Please Enter Your Name:"
+// })
+
+
+// let opponent = await inquirer.prompt({
+//     type:"list",
+//     name:"select",
+//     message:"Select Your Opponent:",
+//     choices:["Skeleton","Assassin","Zombie"]
+    
+
+// })
+
+// // Gather Data
+// let p1 = new Player(player.name)
+// let o1 = new Opponent(opponent.select)
+
+
+// if(opponent.select == "Skeleton"){
+//     console.log(`${chalk.bold.green(p1.name)} VS ${chalk.bold.red(o1.name)}`)
+//     let ask = await inquirer.prompt({
+//         type:"list",
+//         name:"opt",
+//     message:"Select Your Opponent:",
+//     choices: ["Attack", "Drink Portion", "Run For Your Life.."],
+//     });
+//     if()
+// }
